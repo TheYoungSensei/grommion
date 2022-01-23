7d08187c362460fb19c89106a2a39f38c6a39a5a
Christopher Sacré <rk5809@engie.com>
2022-01-23 21:32:38 +0100

    Implement PUT method.

--- api/grommion/Controllers/FeedController.cs
+++ api/grommion/Controllers/FeedController.cs
@@ -9,7 +9,8 @@ namespace FeedAPI.Controllers
   {
     private static List<Feed> feeds = new List<Feed>
       {
-        new Feed { Id = 1, Title = "Test", Description = "Test", Author = "Christopher Sacré"}
+        new Feed { Id = 1, Title = "Test", Description = "Test", Author = "Christopher Sacré"},
+        new Feed { Id = 2, Title = "Test2", Description = "Test2", Author = "Christopher Sacré"}
       };
 
     [HttpGet]
@@ -18,11 +19,34 @@ namespace FeedAPI.Controllers
       return Ok(feeds);
     }
 
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Feed>> Get(int id)
+    {
+      var feed = feeds.Find(f => f.Id == id);
+      if (feed == null)
+        return NotFound("Feed not found");
+      return feed;
+    }
+
     [HttpPost]
     public async Task<ActionResult<Feed>> AddFeed(Feed feed)
     {
       feeds.Add(feed);
       return Ok(feed);
+    }
+
+    [HttpPut]
+    public async Task<ActionResult<Feed>> UpdateFeed(Feed request)
+    {
+      var feed = feeds.Find(f => f.Id == request.Id);
+      if (feed == null)
+        return NotFound("Feed not found");
+      feed.Title = request.Title;
+      feed.Description = request.Description;
+      feed.Author = request.Author;
+
+      return Ok(feed);
+
     }
   }
 }
