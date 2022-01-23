1658346e6fa1f1831d9944937ddd849590c5f2c0
Christopher Sacré <rk5809@engie.com>
2022-01-23 21:24:21 +0100

    Add POST call

--- api/grommion/Controllers/FeedController.cs
+++ api/grommion/Controllers/FeedController.cs
@@ -7,14 +7,22 @@ namespace FeedAPI.Controllers
   [ApiController]
   public class FeedController : ControllerBase
   {
-    [HttpGet]
-    public async Task<ActionResult<List<Feed>>> Get()
-    {
-      var feeds = new List<Feed>
+    private static List<Feed> feeds = new List<Feed>
       {
         new Feed { Id = 1, Title = "Test", Description = "Test", Author = "Christopher Sacré"}
       };
+
+    [HttpGet]
+    public async Task<ActionResult<List<Feed>>> Get()
+    {
       return Ok(feeds);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Feed>> AddFeed(Feed feed)
+    {
+      feeds.Add(feed);
+      return Ok(feed);
     }
   }
 }
