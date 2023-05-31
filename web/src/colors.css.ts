56651bb5821fa4528c100399438fa3f21efa3949
Christopher Sacré <christopher.sacre@equans.com>
2023-05-31 23:04:53 +0200

    [COLLOR] Add new color palette for test.

--- web/src/colors.css.ts
+++ web/src/colors.css.ts
@@ -5,4 +5,11 @@ export const colors = {
   muted: "#f5cc00",
   active: "#004287",
   black: "#000",
+
+  // COLOR PALETTE
+  pink: '#ef476f',
+  yellow: '#ffd166',
+  green: '#06d6a0',
+  lightBlue: '#118ab2',
+  darkBlue: '#073b4c'
 }
