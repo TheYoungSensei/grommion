ad3eb62d88e3639973073155cc4f50045995c60a
Christopher Sacré <christopher.sacre@equans.com>
2023-05-31 23:12:33 +0200

    [REFACTOR] Update the way color code are declared.

--- web/src/colors.css.ts
+++ web/src/colors.css.ts
@@ -1,15 +1,15 @@
+export const white = '#FAFCFF';
+export const pink = '#ef476f';
+export const yellow = '#ffd166';
+export const green = '#06d6a0';
+export const lightBlue = '#118ab2';
+export const darkBlue = '#073b4c';
+
 export const colors = {
-  background: "#ffd500",
+  background: white,
   text: "#005bbb",
   primary: "#005bbb",
   muted: "#f5cc00",
   active: "#004287",
   black: "#000",
-
-  // COLOR PALETTE
-  pink: '#ef476f',
-  yellow: '#ffd166',
-  green: '#06d6a0',
-  lightBlue: '#118ab2',
-  darkBlue: '#073b4c'
 }
